bb9a695b54c35eea7d26ceb6845b6373566f2510
Youssef Mohamed - Krypton1840 <youssefmohamed.1840@gmail.com>
2023-05-07 21:27:20 +0300

    Initial Database Commit and update newAddLandmarkActivity.java onclick listener

---
+++ app/src/main/java/com/example/andriodproject/DatabaseHelper.java
@@ -0,0 +1,66 @@
+package com.example.andriodproject;
+
+import android.content.ContentValues;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.content.Context;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+
+public class DatabaseHelper extends SQLiteOpenHelper{
+
+    private Context context;
+    private static final String database_name = "Landmarks.db";
+    private static final int database_version = 1;
+    private static final String table_name = "landmarks";
+
+    // 5 columns id, name, description, location, image
+    private static final String column_id = "_id";
+    private static final String column_name = "landmark_name";
+    private static final String column_description = "landmark_description";
+    private static final String column_location = "landmark_location";
+    //private static final String column_image = "landmark_image";
+
+
+
+    public DatabaseHelper(@Nullable Context context) {
+        super(context, database_name, null, database_version);
+        this.context = context;
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String query = "CREATE TABLE " + table_name + " (" + column_id + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                column_name + " TEXT NOT NULL, "+ column_description + " Text NOT NULL, " +
+                column_location + " Text NOT NULL);";
+
+        db.execSQL(query);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + table_name);
+        onCreate(db);
+
+    }
+
+    void add_landmark(String name, String description, String location){
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues cv = new ContentValues();
+
+        cv.put(column_name, name);
+        cv.put(column_description, description);
+        cv.put(column_location, location);
+        //cv.put(column_image, image);
+
+        long result = db.insert(table_name, null, cv);
+        if(result == -1){
+            Toast.makeText(context, "Landmark addition failed.", Toast.LENGTH_SHORT).show();
+        }
+        else {
+            Toast.makeText(context, "Landmark added successfully.", Toast.LENGTH_SHORT).show();
+
+        }
+    }
+}
--- app/src/main/java/com/example/andriodproject/newAddLandmarkActivity.java
+++ app/src/main/java/com/example/andriodproject/newAddLandmarkActivity.java
@@ -5,31 +5,38 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 import com.google.android.material.textfield.TextInputLayout;
 
 public class newAddLandmarkActivity extends AppCompatActivity {
 
+    Button save_button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_add_landmark);
         setTitle("Add New Landmark");
 
+        save_button = findViewById(R.id.save_button);
+        save_button.setOnClickListener(v -> {
+            //try
+            Intent j = new Intent(newAddLandmarkActivity.this,MainActivity.class);
+            String landmarkName = ((TextInputLayout)findViewById(R.id.textInputLayout)).getEditText().getText().toString();
+            j.putExtra("NewName",landmarkName);
+            String landmarkDescription = ((TextInputLayout)findViewById(R.id.textInputLayout2)).getEditText().getText().toString();
+            j.putExtra("NewInfo",landmarkDescription);
+            String landmarkLocation = ((TextInputLayout)findViewById(R.id.textInputLayout3)).getEditText().getText().toString();
+            j.putExtra("NewLocation",landmarkLocation);
+            startActivity(j);
+
+            DatabaseHelper myDB = new DatabaseHelper(newAddLandmarkActivity.this);
+            myDB.add_landmark(landmarkName,landmarkDescription, landmarkLocation);
+        });
     }
 
-    public void NewSave(View vi)
-    {
-        Intent j = new Intent(this,MainActivity.class);
-        String landmarkName = ((TextInputLayout)findViewById(R.id.textInputLayout)).getEditText().getText().toString();
-        j.putExtra("NewName",landmarkName);
-        String landmarkInfo = ((TextInputLayout)findViewById(R.id.textInputLayout2)).getEditText().getText().toString();
-        j.putExtra("NewInfo",landmarkInfo);
-        String landmarkLocation = ((TextInputLayout)findViewById(R.id.textInputLayout3)).getEditText().getText().toString();
-        j.putExtra("NewLocation",landmarkLocation);
-        startActivity(j);
-    }
+
     public void Cancel(View v)
     {
         Intent f = new Intent(this,MainActivity.class);
