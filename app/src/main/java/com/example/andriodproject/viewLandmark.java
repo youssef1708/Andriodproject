42ba125802a320e6202645a83d084d8de6bd11d7
youssef1708 <youssefyasser2405@gmail.com>
2023-05-10 19:41:17 +0300

    Get location in view landmark layout added

--- app/src/main/java/com/example/andriodproject/viewLandmark.java
+++ app/src/main/java/com/example/andriodproject/viewLandmark.java
@@ -2,15 +2,25 @@ package com.example.andriodproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class viewLandmark extends AppCompatActivity {
 
     TextView  landmark_Name,landmark_info,landmark_Location;
     String Name,Info,Location;
 
+    FloatingActionButton get_Location;
+
+    @SuppressLint("WrongViewCast")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +29,22 @@ public class viewLandmark extends AppCompatActivity {
         landmark_Name=findViewById(R.id.nameView);
         landmark_info=findViewById(R.id.infoView);
         landmark_Location=findViewById(R.id.LocationView);
+        //get_Location=findViewById(R.id.buttonL);
+        Button map = (Button) findViewById(R.id.buttonL);
+        map.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent in = new Intent(Intent.ACTION_VIEW, Uri.parse("geo: 0,0?q="+ Name +""));
+                in.setPackage("com.google.android.apps.maps");
+                startActivity(in);
+
+
+
+
+
+            }
+        });
+
         getIntentData();
     }
 
